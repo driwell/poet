f6da6b0847c99a1b95ac066b552d31e479466f9b
Driwell <driwell@tuta.io>
2024-11-07 13:05:52 +0000

    add anyhow and create proof of concept of find

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use std::path::PathBuf;
+use anyhow::{Context, Result};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 use clap::{arg, command, value_parser};
 
-fn main() {
+fn main() -> Result<()> {
     let matches = command!()
         .about("Parses exported topics")
         .arg(arg!(-f --find <PATTERN> "Prints out lines with occurrences of a pattern"))
@@ -12,4 +16,25 @@ fn main() {
                 .required(true),
         )
         .get_matches();
+
+    find(
+        matches
+            .get_one::<String>("find")
+            .with_context(|| "no pattern provided")?,
+        matches
+            .get_one::<PathBuf>("path")
+            .with_context(|| "no path provided")?,
+    )
+}
+
+fn find(pattern: &str, path: &Path) -> Result<()> {
+    let content = fs::read_to_string(path)?;
+
+    for line in content.lines() {
+        if line.contains(pattern) {
+            println!("{}", line);
+        }
+    }
+
+    Ok(())
 }
