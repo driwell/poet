a73a9fef88cc8e352d150dbd92570c4b9f6af214
Driwell <driwell@tuta.io>
2024-11-07 11:54:18 +0000

    add clap and create clean command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::path::PathBuf;
+
+use clap::{arg, command, value_parser};
+
 fn main() {
-    todo!();
+    let matches = command!()
+        .about("Parses exported topics")
+        .arg(arg!(-c --clean <FILE> "Prints out a clean file").value_parser(value_parser!(PathBuf)))
+        .get_matches();
+
+    println!(
+        "file: {}",
+        matches.get_one::<PathBuf>("clean").unwrap().display()
+    );
 }
