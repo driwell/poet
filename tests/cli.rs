7e6dce543bddb622409ce84ec9f5a02bd91275d9
Driwell <driwell@tuta.io>
2024-11-08 10:35:04 +0000

    write test for no pattern case

--- tests/cli.rs
+++ tests/cli.rs
@@ -17,13 +17,27 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
 #[test]
 fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
     let file = assert_fs::NamedTempFile::new("sample.txt")?;
-    file.write_str("A fox\njumped over\na foo\nthen a bar\nthen a foobar")?;
+    file.write_str("A foo\njumped over a bar\nthen a foobar")?;
 
     let mut cmd = Command::cargo_bin("poet")?;
     cmd.arg("-f").arg("foo").arg(file.path());
     cmd.assert()
         .success()
-        .stdout(predicate::str::contains("a foo\nthen a foobar"));
+        .stdout(predicate::str::contains("A foo\nthen a foobar"));
+
+    Ok(())
+}
+
+#[test]
+fn prints_content_in_file_without_pattern() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("A foo\njumped over a bar\nthen a foobar")?;
+
+    let mut cmd = Command::cargo_bin("poet")?;
+    cmd.arg(file.path());
+    cmd.assert().success().stdout(predicate::str::contains(
+        "A foo\njumped over a bar\nthen a foobar",
+    ));
 
     Ok(())
 }
