686aeb3c3a821545324eb1fc3275df5de9addcc2
Driwell <driwell@tuta.io>
2024-11-08 13:29:19 +0000

    start implementing buffering

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,20 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
 use anyhow::{Context, Result};
 use std::{fs, path::Path};
 
 pub fn find(pattern: &str, path: &Path) -> Result<()> {
-    let content = fs::read_to_string(path)
-        .with_context(|| format!("could not read file `{}`", path.display()))?;
+    let content = File::open(path)?;
+    let reader = BufReader::new(content);
 
-    for line in content.lines().filter(|line| line.contains(pattern)) {
-        println!("{}", line);
+    // TODO: implement this properly
+    for line in reader.lines() {
+        let un = line?;
+        if un.contains(pattern) {
+            println!("{}", un);
+        }
     }
 
     Ok(())
