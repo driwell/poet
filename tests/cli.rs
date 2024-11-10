116681a966addae780f425105f5dc3442d88f393
Driwell <driwell@tuta.io>
2024-11-10 19:25:25 +0000

    write replace_content_in_file test

--- tests/cli.rs
+++ tests/cli.rs
@@ -41,3 +41,17 @@ fn prints_content_in_file_without_pattern() -> Result<(), Box<dyn std::error::Er
 
     Ok(())
 }
+
+#[test]
+fn replace_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("A foo\njumped over a bar\nthen a foobar")?;
+
+    let mut cmd = Command::cargo_bin("poet")?;
+    cmd.arg("-r").arg("foo").arg("baz").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::eq("A baz\njumped over a bar\nthen a bazbar\n"));
+
+    Ok(())
+}
