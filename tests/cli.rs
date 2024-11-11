f96e45a7c7c068dbad59e504757a71dc5ccbeac4
Driwell <driwell@tuta.io>
2024-11-11 16:40:45 +0000

    write find_and_unfold_content_in_file test

--- tests/cli.rs
+++ tests/cli.rs
@@ -83,3 +83,25 @@ fn print_all_content_when_no_option() -> Result<(), Box<dyn std::error::Error>> 
 
     Ok(())
 }
+
+#[test]
+fn find_and_unfold_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("A foo\njumped over a bar\nthen a foobar")?;
+
+    let frens = assert_fs::NamedTempFile::new("frens.txt")?;
+    frens.write_str("cat\ndog")?;
+
+    let mut cmd = Command::cargo_bin("poet")?;
+    cmd.arg("-f")
+        .arg("foo")
+        .arg(file.path())
+        .arg("-u")
+        .arg("-i")
+        .arg(frens.path());
+    cmd.assert().success().stdout(predicate::eq(
+        "A cat\nA dog\nthen a catbar\nthen a dogbar\n",
+    ));
+
+    Ok(())
+}
